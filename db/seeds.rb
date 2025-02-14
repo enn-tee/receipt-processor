5bd7af006a4c9dd4bb4288882d1742e947d56cf8
Nick Thomas <nthomas@sayrhino.com>
2025-02-14 15:57:06 -0600

    update bin/setup to nuke the db and run seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 Receipt.create!(
+  external_id: 'ce4d1960-f838-424c-896b-e8e426dba1d7',
   retailer: 'Walgreens',
   purchase_date: Date.new(2022, 1, 2),
   purchase_time: Time.new(2022, 1, 2, 8, 13),
@@ -16,6 +17,7 @@ Receipt.create!(
 )
 
 Receipt.create!(
+  external_id: '2f160ce8-e496-4ee0-bd23-2982f7534b1e',
   retailer: 'Target',
   purchase_date: Date.new(2022, 1, 2),
   purchase_time: Time.new(2022, 1, 2, 13, 13),
